e1257978f209c59c5a8268019827f03d887d3626
edivan-car <edivam.c@gmail.com>
2024-03-30 02:15:48 -0300

    Project created

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Projeto: Sistema Jogo de Xadrez
+ * Curso: Java COMPLETO Programação Orientada a Objetos + Projetos
+ * Professor: Nelio Alves
+ * Seção: 16
+ * Aulas: 179 à 211
+ */
+module chess_system {
+}
